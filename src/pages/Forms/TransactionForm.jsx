363a4538ea0f847270a813bc46c637faaa33d1c6
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-08 15:43:44 -0600

    Implementando el useFirestore | TransactionForm

--- src/pages/Forms/TransactionForm.jsx
+++ src/pages/Forms/TransactionForm.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function TransactionForm(){
+export default function TransactionForm({uid}){
     const [name, setName] = useState('');
     const [amount, setAmount] = useState('');
+    const { addDocument, response } = useFirestore('transactions');
 
     const handleSubmit = (e) => {
         e.preventDefault(); 
-        console.log({
-            name, 
-            amount
-        });
+        addDocument({ uid, name, amount });
     }
+
+    useEffect(() => {
+      if(response.sucess){
+        setName('');
+        setAmount('');
+      }
+      
+    }, [response.success])
+    
     return (
         <>
             <h3>Add Transaction</h3>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useAuthContext } from '../../hooks/useAuthContext';
 import TransactionForm from '../Forms/TransactionForm';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const {user} = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         Transaction List
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm/>
+        <TransactionForm uid={user.uid}/>
       </div>
     </div>
   )
