ab3393846c8f2374b8342e7ca5a29111a19d7a4c
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-08 14:03:16 -0600

    Auth state changes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,26 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { projectAuth } from "../firebase/config";
 import AuthReducer from "../reducer/AuthReducer";
+import { AUTH_IS_READY } from "../types/Types";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(AuthReducer, {
-        user: null
+        user: null,
+        authIsReady: false
     });
+
+    useEffect(() => {
+      const unsub = projectAuth.onAuthStateChanged(user => {
+        dispatch({
+          type: AUTH_IS_READY,
+          payload: user
+        });
+        unsub();
+      })
+    }, [])
+    
 
     console.log('AuthContextProvider: state', state);
 
