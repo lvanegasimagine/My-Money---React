6fb4c21f8adc151b63f7d7e1c9a7940fd9416b7e
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-08 14:08:20 -0600

    Implementando authIsReady | AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import { useAuthContext } from "../hooks/useAuthContext";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Signup from "../pages/signup/Signup";
 
 const AppRoutes = () => {
+  const { authIsReady } = useAuthContext();
   return (
     <div className="app">
-      <Router>
-      <Navbar/>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </Switch>
-      </Router>
+      {authIsReady && (
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 };
