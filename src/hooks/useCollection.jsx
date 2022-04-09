a2526cc9be949cf8fe7f0acb3b92b82c1c473c9a
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-09 11:46:22 -0600

    Presenta lista de acuerdo al id del cliente... add query en useCollection

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -1,27 +1,38 @@
-import {useState, useEffect} from 'react';
-import { projectFirestore } from '../firebase/config';
+import { useState, useEffect, useRef } from "react";
+import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection) =>{
-    const [documents, setDocuments] = useState(null);
-    const [error, setError] = useState(null);
+export const useCollection = (collection, _query) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        let ref = projectFirestore.collection(collection);
+  //Todo: if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+  
+  const query = useRef(_query).current;
 
-        const unsubscribe = ref.onSnapshot((snapshot) => {
-            let results = [];
-            snapshot.docs.forEach((doc) => {
-                results.push({...doc.data(), id: doc.id});
-            })
-            setDocuments(results);
-            setError(null)
-        }, (error) =>{
-            console.log(error);
-            setError('Could not fetch documents');
-        })
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
 
-        return () => unsubscribe();
-    },[collection]);
+    if(query){
+        ref = ref.where(...query);
+    }
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError("Could not fetch documents");
+      }
+    );
 
-    return {documents, error};
-}
+    return () => unsubscribe();
+  }, [collection, query]);
+
+  return { documents, error };
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("transactions");
+  const { documents, error } = useCollection("transactions",['uid','==', user.uid]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
