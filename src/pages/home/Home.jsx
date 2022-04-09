4aa518c5643395b6714f404dec8861093cbccb20
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-09 11:39:40 -0600

    List Transaction, add useCollection | TransactionList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import TransactionForm from '../Forms/TransactionForm';
-import styles from './Home.module.css';
+import React from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionForm from "../Transaction/TransactionForm";
+import TransactionList from "../Transaction/TransactionList";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const {user} = useAuthContext();
-
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions");
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        Transaction List
+      {error && <p>{error}</p>}
+      {documents && <TransactionList transactions={documents}/>}
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm uid={user.uid}/>
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
